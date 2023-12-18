ae978a35acf769dd3ba149eba3f190658b273639
Tofu0mega <110778633+Tofu0mega@users.noreply.github.com>
2023-12-18 17:07:43 +0545

    Basic Functions expanded

--- econfunctions.py
+++ econfunctions.py
@@ -24,3 +24,6 @@ def AER(k,n):
     i=k/100
     val=((1+k/n)**n)-1
     return round(val,5)
+
+def help():
+    msg="Available Functions :\n FV(k,n),PV(k,n),FVA(k,n),PVA(k,n),AER(k,n)\n UseExample(For future value of initial payment of 1000 with 200 annuity after 5 years at 10%:1000*FV(10,5)+200*FVA(10,5)"
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 # Get user input for the function call
 while True:
     try:
-        user_input = input("Enter String(Ctrl+C ) To Break: ")
+        user_input = input("Enter String(Ctrl+C ) To Break use help() for introduction: ")
         # Use eval to execute the user input
         result = eval(user_input)
         print("Result:", np.ceil(result))
